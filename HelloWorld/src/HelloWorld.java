767abb06f8181e07193a32cce4338f018439217d
tomekroweer <74041441+tomekroweer@users.noreply.github.com>
2020-11-20 13:39:09 +0100

    Add files via upload

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class  HelloWorld {
+    public static void main(String [] args){
+        int a = 12;
+        double f = 4.5;
+        float af = 5.6f;
+        String s = "hello world ";
+        boolean b = true;
+        int[] array = {1,2,3};
+
+
+        final int c;
+        c = 19;
+        System.out.println("wynik: " +s);
+
+    }
+}
